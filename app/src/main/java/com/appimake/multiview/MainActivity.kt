67ad57734acd40524dc2330e4b6c4711df5434a9
Nattapong Rattasamut <raatasamut@gmail.com>
2019-06-21 13:30:35 +0700

    exclude cellpool

--- app/src/main/java/com/appimake/multiview/MainActivity.kt
+++ app/src/main/java/com/appimake/multiview/MainActivity.kt
@@ -5,11 +5,6 @@ import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.ItemTouchHelper
-import com.appimake.cellpool.cell.card.ImageCardData
-import com.appimake.cellpool.cell.card.ImageCardModel
-import com.appimake.cellpool.cell.gallery.ViewGalleryData
-import com.appimake.cellpool.cell.gallery.ViewGalleryDataModel
-import com.appimake.cellpool.cell.title.ViewTitleData
 import com.appimake.multirecyclerview.*
 
 
@@ -24,45 +19,45 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val cellList = arrayListOf<BaseMultiViewData>()
-        cellList.add(ViewTitleData("Gallery"))
-        cellList.add(ViewGalleryData(ViewGalleryDataModel(arrayListOf(
-                "https://images.pexels.com/photos/257360/pexels-photo-257360.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
-                "https://images.pexels.com/photos/443446/pexels-photo-443446.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
-                "https://images.pexels.com/photos/490411/pexels-photo-490411.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
-                "https://images.pexels.com/photos/257360/pexels-photo-257360.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"))))
-
-        cellList.add(ViewTitleData("Picture"))
-        cellList.add(ImageCardData(ImageCardModel("https://images.pexels.com/photos/257360/pexels-photo-257360.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260", "Image")))
-
-        viewManager = androidx.recyclerview.widget.LinearLayoutManager(this)
-        viewAdapter = MultiViewAdapter(this, cellList)
-        recyclerView = findViewById<androidx.recyclerview.widget.RecyclerView>(R.id.main_recycler_view).apply {
-            layoutManager = viewManager
-            adapter = viewAdapter
-            setHasFixedSize(true)
-        }
-
-        // Set swipe left right
-
-        val multiSwipeController = MultiSwipeController(recyclerView, object : MultiSwipeControllerActions() {
-            override fun onLeftClicked(position: Int, data: BaseMultiViewData) {
-                super.onLeftClicked(position, data)
-                Log.d("Swipe", "Left on position : $position")
-            }
-
-            override fun onRightClicked(position: Int, data: BaseMultiViewData) {
-                super.onRightClicked(position, data)
-                Log.d("Swipe", "Right on position : $position")
-            }
-        }, ItemTouchHelper.LEFT or ItemTouchHelper.RIGHT)
-
-        multiSwipeController.apply {
-            buttonWidth = 300f
-            buttonMargin = 22f
-            buttonLeft = SwipeButton("ANT", Color.BLACK, 30f)
-            buttonRight = SwipeButton("DOG", Color.MAGENTA, 30f)
-        }
+//        val cellList = arrayListOf<BaseMultiViewData>()
+//        cellList.add(ViewTitleData("Gallery"))
+//        cellList.add(ViewGalleryData(ViewGalleryDataModel(arrayListOf(
+//                "https://images.pexels.com/photos/257360/pexels-photo-257360.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+//                "https://images.pexels.com/photos/443446/pexels-photo-443446.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+//                "https://images.pexels.com/photos/490411/pexels-photo-490411.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+//                "https://images.pexels.com/photos/257360/pexels-photo-257360.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"))))
+//
+//        cellList.add(ViewTitleData("Picture"))
+//        cellList.add(ImageCardData(ImageCardModel("https://images.pexels.com/photos/257360/pexels-photo-257360.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260", "Image")))
+//
+//        viewManager = androidx.recyclerview.widget.LinearLayoutManager(this)
+//        viewAdapter = MultiViewAdapter(this, cellList)
+//        recyclerView = findViewById<androidx.recyclerview.widget.RecyclerView>(R.id.main_recycler_view).apply {
+//            layoutManager = viewManager
+//            adapter = viewAdapter
+//            setHasFixedSize(true)
+//        }
+//
+//        // Set swipe left right
+//
+//        val multiSwipeController = MultiSwipeController(recyclerView, object : MultiSwipeControllerActions() {
+//            override fun onLeftClicked(position: Int, data: BaseMultiViewData) {
+//                super.onLeftClicked(position, data)
+//                Log.d("Swipe", "Left on position : $position")
+//            }
+//
+//            override fun onRightClicked(position: Int, data: BaseMultiViewData) {
+//                super.onRightClicked(position, data)
+//                Log.d("Swipe", "Right on position : $position")
+//            }
+//        }, ItemTouchHelper.LEFT or ItemTouchHelper.RIGHT)
+//
+//        multiSwipeController.apply {
+//            buttonWidth = 300f
+//            buttonMargin = 22f
+//            buttonLeft = SwipeButton("ANT", Color.BLACK, 30f)
+//            buttonRight = SwipeButton("DOG", Color.MAGENTA, 30f)
+//        }
 
         // End. Set swipe left right
     }
--- app/src/main/java/com/appimake/multiview/MainAsView.kt
+++ app/src/main/java/com/appimake/multiview/MainAsView.kt
@@ -1,19 +1,10 @@
 package com.appimake.multiview
 
-import android.app.Activity
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.appimake.cellpool.cell.gallery.ViewGalleryData
-import com.appimake.cellpool.cell.gallery.ViewGalleryDataModel
-import com.appimake.cellpool.cell.title.ViewTitleData
-import com.appimake.multirecyclerview.toView
-import com.appimake.multiview.conponent.cell_001.Cell001DataModel
-import com.appimake.multiview.conponent.cell_001.DataCell_001
-import kotlinx.android.synthetic.main.activity_view.*
+import androidx.lifecycle.MutableLiveData
 
-class MainAsView: AppCompatActivity() {
+class MainAsView : AppCompatActivity() {
 
     val testValue: MutableLiveData<Int> by lazy {
         MutableLiveData<Int>()
@@ -23,27 +14,27 @@ class MainAsView: AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_view)
 
-        activity_view_cn.addView(ViewTitleData("Map").toView(this))
-        activity_view_cn.addView(ViewGalleryData(ViewGalleryDataModel(arrayListOf(
-                "https://images.pexels.com/photos/257360/pexels-photo-257360.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
-                "https://images.pexels.com/photos/443446/pexels-photo-443446.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
-                "https://images.pexels.com/photos/490411/pexels-photo-490411.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
-                "https://images.pexels.com/photos/257360/pexels-photo-257360.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"))).toView(this))
-
-        testValue.value = 3
-
-        activity_view_cn.addView(DataCell_001(Cell001DataModel("Test", testValue)).toView(this))
-        activity_view_cn.addView(DataCell_001(Cell001DataModel("Test", testValue)).toView(this))
-        activity_view_cn.addView(DataCell_001(Cell001DataModel("Test", testValue)).toView(this))
-        activity_view_cn.addView(DataCell_001(Cell001DataModel("Test", testValue)).toView(this))
-        activity_view_cn.addView(DataCell_001(Cell001DataModel("Test", testValue)).toView(this))
-        activity_view_cn.addView(DataCell_001(Cell001DataModel("Test", testValue)).toView(this))
-        activity_view_cn.addView(DataCell_001(Cell001DataModel("Test", testValue)).toView(this))
-        activity_view_cn.addView(DataCell_001(Cell001DataModel("Test", testValue)).toView(this))
-        activity_view_cn.addView(DataCell_001(Cell001DataModel("Test", testValue)).toView(this))
-        activity_view_cn.addView(DataCell_001(Cell001DataModel("Test", testValue)).toView(this))
-        activity_view_cn.addView(DataCell_001(Cell001DataModel("Test", testValue)).toView(this))
-        activity_view_cn.addView(DataCell_001(Cell001DataModel("Test", testValue)).toView(this))
-        activity_view_cn.addView(DataCell_001(Cell001DataModel("Test", testValue)).toView(this))
+//        activity_view_cn.addView(ViewTitleData("Map").toView(this))
+//        activity_view_cn.addView(ViewGalleryData(ViewGalleryDataModel(arrayListOf(
+//                "https://images.pexels.com/photos/257360/pexels-photo-257360.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+//                "https://images.pexels.com/photos/443446/pexels-photo-443446.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+//                "https://images.pexels.com/photos/490411/pexels-photo-490411.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+//                "https://images.pexels.com/photos/257360/pexels-photo-257360.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"))).toView(this))
+//
+//        testValue.value = 3
+//
+//        activity_view_cn.addView(DataCell_001(Cell001DataModel("Test", testValue)).toView(this))
+//        activity_view_cn.addView(DataCell_001(Cell001DataModel("Test", testValue)).toView(this))
+//        activity_view_cn.addView(DataCell_001(Cell001DataModel("Test", testValue)).toView(this))
+//        activity_view_cn.addView(DataCell_001(Cell001DataModel("Test", testValue)).toView(this))
+//        activity_view_cn.addView(DataCell_001(Cell001DataModel("Test", testValue)).toView(this))
+//        activity_view_cn.addView(DataCell_001(Cell001DataModel("Test", testValue)).toView(this))
+//        activity_view_cn.addView(DataCell_001(Cell001DataModel("Test", testValue)).toView(this))
+//        activity_view_cn.addView(DataCell_001(Cell001DataModel("Test", testValue)).toView(this))
+//        activity_view_cn.addView(DataCell_001(Cell001DataModel("Test", testValue)).toView(this))
+//        activity_view_cn.addView(DataCell_001(Cell001DataModel("Test", testValue)).toView(this))
+//        activity_view_cn.addView(DataCell_001(Cell001DataModel("Test", testValue)).toView(this))
+//        activity_view_cn.addView(DataCell_001(Cell001DataModel("Test", testValue)).toView(this))
+//        activity_view_cn.addView(DataCell_001(Cell001DataModel("Test", testValue)).toView(this))
     }
 }
